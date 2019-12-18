3c24239cccfbd48cbf8c9b74d890a94af579dbbd
liuliang <liuliang@111.com.cn>
2019-12-18 10:49:20 +0800

    a simple homepage

---
+++ iplaycodex-homepage/src/components/homePage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <h2>About me</h2>
+        <ul>
+            <li>
+                <a href="https://blog.iplaycodex.com" target="_blank">My blog</a>
+                <a href="https://github.com/wawsc5354524" target="_blank">My github</a>
+                <a href="https://github.com/wawsc5354524" target="_blank">My open source project</a>
+            </li>
+            <br />
+        </ul>
+        <!-- 备案号 -->
+        <a  class="copy-right" href="http://www.beian.miit.gov.cn/">沪ICP备19045350号</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "homePage",
+    data() {
+        return {
+            msg: "Welcome to My website"
+        };
+    }
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+    font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+a {
+    color: #42b983;
+    margin: 0 10px;
+}
+</style>
