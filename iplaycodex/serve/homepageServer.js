5bff43fc85b2062287fbdabdcec9fed879755e61
liuliang <liuliang@111.com.cn>
2020-06-10 16:26:25 +0800

    修改服务端的path

--- iplaycodex/serve/homepageServer.js
+++ iplaycodex/serve/homepageServer.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 // 部署上线时读取静态文件
-app.use(express.static(path.join(__dirname, '../')));
+app.use(express.static(path.join(__dirname, '../client')));
 
 // 博客监听 3000 端口
 app.listen(3000);
